df367d413f3d4081146ea09fd33868701aabc046
Valentin Duboscq <valentin@octopulse.io>
2018-10-27 15:20:26 +0200

    docker compose api node

---
+++ docker/api/app.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('Epitech project : API in NodeJS')
+})
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
+
